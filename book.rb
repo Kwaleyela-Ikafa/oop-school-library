31381905d250798a08a1670b574298ffd0602a6d
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-11-02 20:51:44 +0200

    make-required-changes

--- book.rb
+++ book.rb
@@ -1,6 +1,5 @@
 class Book
-  attr_accessor :title, :author
-  attr_reader :rental
+  attr_accessor :title, :author, :rental
 
   def initialize(title, author)
     @title = title
@@ -8,7 +7,7 @@ class Book
     @rental = []
   end
 
-  def add_rental(rental)
+  def add_rental
     @rental << rental
   end
 end
