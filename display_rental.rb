5d5d858927b83728f96d5c3fdef9c9fef1eb9413
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-11-08 17:38:15 +0200

    Add-additional-functionality

--- display_rental.rb
+++ display_rental.rb
@@ -1,14 +1,20 @@
 module DisplayRental
   def list_rentals
-    puts 'Select person by ID: '
+    puts 'Select person by ID'
     @people.each do |person|
       puts "ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
     end
+    print 'Enter ID: '
     id = gets.chomp.to_i
+    rental = @rentals.select { |rent| rent.person.id == id }
 
-    puts 'Rentals:'
-    @rentals.each do |rental|
-      puts "Date: #{rental.date}, Book: #{rental.book.title}' by #{rental.book.author}" if rental.person.id == id
+    if rental.empty?
+      puts 'Sorry, there is no rental with that id'
+    else
+      puts 'Rentals:'
+      @rentals.each do |rented|
+        puts "Date: #{rented.date}, Book: #{rented.book.title}' by #{rented.book.author}" if rented.person.id == id
+      end
     end
   end
 end
