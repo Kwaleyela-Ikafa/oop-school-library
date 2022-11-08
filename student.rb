3766f4334081d59d493dfbf7086108507c8639e4
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-11-08 12:44:18 +0200

    Fix-Linter

--- student.rb
+++ student.rb
@@ -4,10 +4,12 @@ require './classroom'
 class Student < Person
   attr_accessor :classroom
 
+  # rubocop:disable Lint/UnusedMethodArgument
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: true)
     @classroom = classroom
   end
+  # rubocop:enable Lint/UnusedMethodArgument
 
   # rubocop:disable Lint/DuplicateMethods
   def classroom=(classroom)
